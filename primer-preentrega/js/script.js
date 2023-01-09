b23c20b489ae746817a2fe53e9e7aaf7e72a5482
Camila Belen Castro <ccastrocami1@gmail.com>
2023-01-08 23:17:46 -0300

    asd

--- primer-preentrega/js/script.js
+++ primer-preentrega/js/script.js
@@ -13,3 +13,5 @@ if (entrada == "") {
 // console.log("Hola, bienvenido a mi JavaScript");let nombre = "";nombre = prompt("Ingrese el nombre")// console.log("NOMBRE INGRESADO:"+" "+nombre)if(nombre == ""){alert("INGRESE UN VALOR")}else{alert("NOMBRE INGRESADO:"+" "+nombre)}let monto = Number(prompt("Ingrese el valor"))let monto2 = Number(prompt("ingrese valor 2"))let total = monto+monto2console.log(total)if(total < 500){alert("Aun no superas los 500")}else if(total < 1000){alert("Bien! ya superaste los 500")}else{alert("VALOR INGRESADO:"+" "+total+ " Bien! superaste los 1000!")}let edad = Number(prompt("ingrese edad"))if(edad > 16 && edad < 65){console.log("esta entre 10 y 50")} else {console.log("NO esta entre 10 y 50")}
 
 // https://www.w3schools.com/js/js_typeof.asp
+
+pusimos github con cami

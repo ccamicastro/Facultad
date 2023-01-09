0beb56cea7270e86a8a0784f2f2743b3c0d15225
Camila Belen Castro <ccastrocami1@gmail.com>
2023-01-08 21:05:17 -0300

    Añadimos github con mi cc

---
+++ primer-preentrega/js/script.js
@@ -0,0 +1,15 @@
+let entrada = prompt("Nombre")+prompt("Apellido")+prompt("Edad");
+const ESPACIO = ""
+if (entrada == "") {
+    alert("ERROR AL INGRESAR")
+}else{
+    alert("DATOS INGRESADOS:"+" "+entrada)
+} 
+    
+// console.log("Hola, bienvenido a mi JavaScript");let nombre = "";let edad;nombre = prompt("Ingrese el nombre")// console.log("NOMBRE INGRESADO:"+" "+nombre)if(nombre == ""){alert("INGRESE UN VALOR")}else{alert("NOMBRE INGRESADO:"+" "+nombre)}let monto = Number(prompt("Ingrese el valor"))let monto2 = Number(prompt("ingrese valor 2"))let total = monto+monto2// console.log(total)if(total < 500){alert("Aun no superas los 500")}else if(total < 1000){alert("Bien! ya superaste los 500")}else{alert("VALOR INGRESADO:"+" "+total+ " Bien! superaste los 1000!")}
+
+
+
+// console.log("Hola, bienvenido a mi JavaScript");let nombre = "";nombre = prompt("Ingrese el nombre")// console.log("NOMBRE INGRESADO:"+" "+nombre)if(nombre == ""){alert("INGRESE UN VALOR")}else{alert("NOMBRE INGRESADO:"+" "+nombre)}let monto = Number(prompt("Ingrese el valor"))let monto2 = Number(prompt("ingrese valor 2"))let total = monto+monto2console.log(total)if(total < 500){alert("Aun no superas los 500")}else if(total < 1000){alert("Bien! ya superaste los 500")}else{alert("VALOR INGRESADO:"+" "+total+ " Bien! superaste los 1000!")}let edad = Number(prompt("ingrese edad"))if(edad > 16 && edad < 65){console.log("esta entre 10 y 50")} else {console.log("NO esta entre 10 y 50")}
+
+// https://www.w3schools.com/js/js_typeof.asp
